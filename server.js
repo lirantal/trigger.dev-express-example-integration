cdabe7bea22057145fb054d934eefa8654d2c800
Liran Tal <liran.tal@gmail.com>
2023-08-11 01:41:20 +0300

    feat: implement the /api/trigger endpoint for the trigger.dev integration
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- server.js
+++ server.js
@@ -32,6 +32,51 @@ client.defineJob({
   },
 });
 
+// Defines the /api/trigger endpoint which Trigger.dev
+// requires to be able to interact and exchange job data
+// with this Node.js server, such as:
+// 1. Register jobs that we defined above with client.defineJob()
+// 2. Trigger jobs when a new event is received
+app.post("/api/trigger", async (req, res, next) => {
+  if (req.method === "HEAD") {
+    return res.sendStatus(200);
+  }
+
+  try {
+    // Prepare the request to be a fetch-compatible Request object:
+    const requestHeaders = req.headers;
+    const requestMethod = req.method;
+    const responseHeaders = {};
+
+    for (const [headerName, headerValue] of Object.entries(requestHeaders)) {
+      responseHeaders[headerName] = headerValue;
+    }
+
+    // Create a new Request object to be passed to the TriggerClient
+    // where we pass the clone the incoming request metadata such as
+    // headers, method, body.
+    const request = new Request("https://express.js/api/trigger", {
+      headers: responseHeaders,
+      method: requestMethod,
+      body: req.body ? JSON.stringify(req.body) : JSON.stringify({}),
+    });
+
+    // This handshake handler knows how to authenticate requests,
+    // call the run() function of the job, and so on
+    const response = await client.handleRequest(request);
+
+    if (!response) {
+      return res.status(404).json({ error: "Not found" });
+    }
+
+    // Optionally can do something with the job's finished
+    // execution's response body
+    return res.status(response.status).json(response.body);
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
