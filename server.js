0ad0b0fda6bf271cc6615c381ef606d74ebd175c
Liran Tal <liran.tal@gmail.com>
2023-08-11 01:33:30 +0300

    feat: add initial trigger.dev integration by adding the SDK and instantiating a new trigger client instance, given an API key
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const { TriggerClient, eventTrigger } = require("@trigger.dev/sdk");
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Instantiate the Trigger.dev client
+const client = new TriggerClient({
+  id: "trigger-express-example-vanilla",
+  apiKey: process.env.TRIGGER_API_KEY,
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
