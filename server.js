d063728b9d3eab0e3caec9ca7e72947f59cf2abb
Liran Tal <liran.tal@gmail.com>
2023-08-11 01:30:11 +0300

    feat: enable json and query params parsing
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
